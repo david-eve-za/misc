9d2fed745f242779d53667ab1e97765f028d3dc0
david-eve-za <david.eve.za@gmail.com>
2018-01-23 15:23:16 -0600

    Update Mega-NG-Compile.sh

--- Mega-NG-Compile.sh
+++ Mega-NG-Compile.sh
@@ -1,3 +1,7 @@
+sudo apt update && sudo apt upgrade
 sudo apt install build-essential
 git clone https://github.com/meganz/sdk
 cd sdk
+sh autogen.sh
+./configure --enable-java --with-java-include-dir=/usr/lib/jvm/java-8-openjdk-amd64/include
+make
