68fa97a49a341bb209ed5cb65643d17283681b44
david-eve-za <david.eve.za@gmail.com>
2018-01-23 16:19:37 -0600

    Update Mega-NG-Compile.sh

--- Mega-NG-Compile.sh
+++ Mega-NG-Compile.sh
@@ -4,6 +4,6 @@ git clone https://github.com/meganz/sdk
 cd sdk
 sh autogen.sh
 sudo apt install swig2.0 libcrypto++-dev libsodium-dev zlib1g-dev libsqlite3-dev libssl-dev libc-ares-dev -y
-sudo apt install libcurl4-openssl-dev libfreeimage-dev libreadline6-dev -y
+sudo apt install libcurl4-openssl-dev libfreeimage-dev libreadline-dev -y
 ./configure --enable-java --with-java-include-dir=/usr/lib/jvm/java-8-openjdk-amd64/include
 make
