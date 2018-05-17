a1fbf3b50dd04f59cbd1e9cbdacb0b430a3a3cf2
david-eve-za <david.eve.za@gmail.com>
2018-05-17 13:43:18 -0500

    Update WPA-WPA2Crack.sh

--- WPA-WPA2Crack.sh
+++ WPA-WPA2Crack.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/bash
 apt update && apt upgrade
 #Install nescesary applications
-apt install aircrack-ng crunch libssl-dev libpcap0.8-dev autoconf automake build-essential cmake -y
+apt install aircrack-ng crunch libssl-dev libpcap0.8-dev autoconf automake build-essential cmake python-dev zlib-devel -y
 #Download CowPatty
 wget http://www.willhackforsushi.com/code/cowpatty/4.6/cowpatty-4.6.tgz
 tar zxfv cowpatty-4.6.tgz
 cd cowpatty-4.6
 make cowpatty
 sudo cp cowpatty /usr/bin
-
+#install Pyrit
+git clone https://github.com/JPaulMora/Pyrit.git
+pip install psycopg2
+pip install scapy
+cd Pyrit
+python setup.py clean
+python setup.py build
+python setup.py install
