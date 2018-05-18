f73a1636b7471fc143de6d8f248378b53f12df00
david-eve-za <david.eve.za@gmail.com>
2018-05-18 10:04:25 -0500

    Update WPA-WPA2Crack.sh

--- WPA-WPA2Crack.sh
+++ WPA-WPA2Crack.sh
@@ -7,14 +7,14 @@ wget http://www.willhackforsushi.com/code/cowpatty/4.6/cowpatty-4.6.tgz
 tar zxfv cowpatty-4.6.tgz
 cd cowpatty-4.6
 make
-sudo cp cowpatty /usr/bin
-sudo cp genpmk /usr/bin
+cp cowpatty /usr/bin
+cp genpmk /usr/bin
 cd ..
 rm -vRf cowpatty-4.6*
 #install Pyrit
 git clone https://github.com/JPaulMora/Pyrit.git
-sudo pip install --upgrade pip
-sudo pip install --upgrade virtualenv
+pip install --upgrade pip
+pip install --upgrade virtualenv
 pip install psycopg2
 pip install scapy
 cd Pyrit
