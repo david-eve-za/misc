0e98281b5e8ff40fefeadd4de61dc6d2fa785890
david-eve-za <david.eve.za@gmail.com>
2018-06-20 12:16:07 -0500

    Update InstallHerokuApp.sh

--- InstallHerokuApp.sh
+++ InstallHerokuApp.sh
@@ -1,4 +1,5 @@
 sudo add-apt-repository "deb https://cli-assets.heroku.com/branches/stable/apt ./"
 curl -L https://cli-assets.heroku.com/apt/release.key | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install heroku
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install heroku -y
