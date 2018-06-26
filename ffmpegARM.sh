75e4e2a08a0113f5c7cdba24679ad57c0203f819
david-eve-za <david.eve.za@gmail.com>
2018-06-26 14:05:54 -0500

    Update ffmpegARM.sh

--- ffmpegARM.sh
+++ ffmpegARM.sh
@@ -13,18 +13,24 @@ NDK=$(pwd)/android-ndk-r17b
 echo $NDK
 SYSROOT=$NDK/platforms/android-16/arch-arm/
 echo $SYSROOT
-TOOLCHAIN=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64
+TOOLCHAIN=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64
 echo $TOOLCHAIN
-
-function build_one
-{
-./configure --prefix=$PREFIX --disable-shared --enable-gpl --enable-static --disable-doc --enable-ffmpeg --disable-ffplay --disable-ffprobe --disable-ffserver --enable-avdevice --disable-doc --disable-symver --cross-prefix=$TOOLCHAIN/bin/arm-linux-androideabi- --target-os=linux --arch=armv7 --enable-cross-compile --sysroot=$SYSROOT --extra-cflags="-Os -fPIE $ADDI_CFLAGS" --extra-ldflags="-pie $ADDI_LDFLAGS" $ADDITIONAL_CONFIGURE_FLAG
-make clean
-make
-make install
-}
 CPU=armv7-a
 PREFIX=$(pwd)/android/$CPU
 echo $PREFIX
 ADDI_CFLAGS="-march=armv7-a"
-#build_one
+#Cloning ffmpeg repo
+git clone git://source.ffmpeg.org/ffmpeg.git
+cd ffmpeg
+#Installing Essentials
+sudo apt-get install mercurial cmake cmake-curses-gui build-essential gcc-arm-linux-gnueabi g++-arm-linux-gnueabi -y
+hg clone https://bitbucket.org/multicoreware/x265
+cd x265/build/arm-linux
+sh make-Makefiles.bash
+make
+cd ../../../
+echo $(pwd)
+#./configure --prefix=$PREFIX --disable-shared --enable-gpl --enable-static --disable-doc --enable-ffmpeg --disable-ffplay --disable-ffprobe --disable-ffserver --enable-avdevice --disable-doc --disable-symver --cross-prefix=$TOOLCHAIN/bin/arm-linux-androideabi- --target-os=linux --arch=armv7 --enable-cross-compile --sysroot=$SYSROOT --extra-cflags="-Os -fPIE $ADDI_CFLAGS" --extra-ldflags="-pie $ADDI_LDFLAGS" $ADDITIONAL_CONFIGURE_FLAG
+#make clean
+#make
+#make install
