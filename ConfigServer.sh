893a3be77c7896c6a462d033235da5c68854041d
david-eve-za <david.eve.za@gmail.com>
2018-09-21 11:46:50 -0400

    Update ConfigServer.sh

--- ConfigServer.sh
+++ ConfigServer.sh
@@ -1,7 +1,7 @@
 #Enable autostart on boot for network
-sed -i 's/ONBOOT=no/ONBOOT=yes/g' /etc/sysconfig/network-scripts/ifcfg-eth*
+#sed -i 's/ONBOOT=no/ONBOOT=yes/g' /etc/sysconfig/network-scripts/ifcfg-eth*
 #Enable repositories
-subscription-manager register --username $1 --password $2 --auto-attach
+#subscription-manager register --username $1 --password $2 --auto-attach
 #Update the python version to 3.7.0
 yum autoremove python.x86_64
 yum -y groupinstall development
@@ -10,8 +10,10 @@ wget https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tar.xz
 tar xJf Python-3.7.0.tar.xz
 cd Python-3.7.0
 ./configure
-make make install
+make 
+make install
 #Download Avalon
+cd ..
 wget http://highqsoft.com/public/software/AvalonSuite-4.7-833-Linux64-JacORB.tgz
 wget http://highqsoft.com/public/software/AvalonSuite-4.7-833-Linux64-SunORB.tgz
 #Uninstall old java versions
