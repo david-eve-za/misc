7af91e75e73a9c05a98dff58993a061164eda99d
david-eve-za <david.eve.za@gmail.com>
2018-09-21 11:35:14 -0400

    Update ConfigServer.sh

--- ConfigServer.sh
+++ ConfigServer.sh
@@ -3,6 +3,7 @@ sed -i 's/ONBOOT=no/ONBOOT=yes/g' /etc/sysconfig/network-scripts/ifcfg-eth*
 #Enable repositories
 subscription-manager register --username $1 --password $2 --auto-attach
 #Update the python version to 3.7.0
+yum autoremove python.x86_64
 yum -y groupinstall development
 yum -y install zlib-devel
 wget https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tar.xz
@@ -10,3 +11,10 @@ tar xJf Python-3.7.0.tar.xz
 cd Python-3.7.0
 ./configure
 make make install
+#Download Avalon
+wget http://highqsoft.com/public/software/AvalonSuite-4.7-833-Linux64-JacORB.tgz
+wget http://highqsoft.com/public/software/AvalonSuite-4.7-833-Linux64-SunORB.tgz
+#Uninstall old java versions
+yum autoremove java-1.6.0-openjdk.x86_64
+yum autoremove java-1.7.0-openjdk.x86_64
+wget http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jdk-8u181-linux-x64.rpm
