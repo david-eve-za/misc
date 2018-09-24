9361543087d1bbca742bd51a57f09fa7e3dc1878
david-eve-za <david.eve.za@gmail.com>
2018-09-24 10:33:28 -0400

    Update ConfigServer.sh

--- ConfigServer.sh
+++ ConfigServer.sh
@@ -3,7 +3,7 @@
 #Enable repositories
 #subscription-manager register --username $1 --password $2 --auto-attach
 #Update the python version to 3.7.0
-yum autoremove python.x86_64
+yum erase python.x86_64
 yum -y groupinstall development
 yum -y install zlib-devel
 wget https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tar.xz
@@ -17,6 +17,15 @@ cd ..
 wget http://highqsoft.com/public/software/AvalonSuite-4.7-833-Linux64-JacORB.tgz
 wget http://highqsoft.com/public/software/AvalonSuite-4.7-833-Linux64-SunORB.tgz
 #Uninstall old java versions
-yum autoremove java-1.6.0-openjdk.x86_64
-yum autoremove java-1.7.0-openjdk.x86_64
-wget http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jdk-8u181-linux-x64.rpm
+yum erase java-1.6.0-openjdk.x86_64
+yum erase java-1.7.0-openjdk.x86_64
+#Install new Java Version
+cd /opt/
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jdk-8u181-linux-x64.tar.gz"
+tar xzf jdk-8u181-linux-x64.tar.gz
+rm jdk-8u181-linux-x64.tar.gz
+cd /opt/jdk1.8.0_181/
+alternatives --install /usr/bin/java java /opt/jdk1.8.0_181/bin/java 2
+alternatives --config java
+#Install Oracle 12c
+wget https://download.oracle.com/otn/linux/oracle12c/122010/linuxx64_12201_database.zip
